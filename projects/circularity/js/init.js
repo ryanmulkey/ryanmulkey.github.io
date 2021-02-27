549738346fbdb99357b24287745568edb574deda
ryanmulkey <ryanmulkey17@gmail.com>
2021-02-27 05:53:58 +0000

    added runtime

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -54,10 +54,9 @@ var init = function (window) {
         }
         
             // TODO 5 : Call game.checkCirclePosition() on your circles.
-           
 
             // TODO 8 : Iterate over the array
-    }  
+    }
     
         /* 
         This Function should check the position of a circle that is passed to the 
