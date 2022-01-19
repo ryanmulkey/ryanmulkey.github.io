28556aac0aa28f48817b1607a7097adb96729ab7
ryanmulkey <ryanmulkey17@gmail.com>
2022-01-19 17:57:41 +0000

    todo 1

--- projects/sorting/index.js
+++ projects/sorting/index.js
@@ -23,7 +23,13 @@ The CSS ids you will work with are:
 
 
 // TODO 1: Implement swap
+function swap (array, i, j) {
+    var temp = array[i];
+    array[i] = array[j]; 
+    array[j] = temp;
 
+    drawSwap(array, i, j);
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
