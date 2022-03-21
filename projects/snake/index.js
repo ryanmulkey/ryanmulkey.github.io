91b62e2187ecd75565f753de2d08bce7d9a59c98
ryanmulkey <ryanmulkey17@gmail.com>
2022-03-21 17:21:49 +0000

    snake push

--- projects/snake/index.js
+++ projects/snake/index.js
@@ -6,9 +6,11 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  let grid = document.querySelector(".grid");
+  let popup = document.querySelector(".popup");
 
   // Constant Variables
-  var FRAME_RATE = 60;
+  var FRAME_RATE = 10;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   var KEY = {
     "ENTER": 13,
@@ -25,33 +27,32 @@ function runProgram(){
     
   };
   // Game Item Objects
-  var snake = {};
-  snake.x = 0;
-  snake.y = 0;
-  snake.speedX = 0;
-  snake.speedY = 0;
-  snake.id = "#snake";
-  snake.it = true; 
-  snake.points = 0;
-
-  var apple = {};
-  apple.x = 50;
-  apple.y = 50;
-  apple.speedX = 0;
-  apple.speedY = 0;
-  apple.id = "#apple";
+  function Factory (id, x, y, speedX, speedY) {
+    var instance = {};
+    
+      instance.id = id;
+      instance.x = x;
+      instance.y = y;
+      instance.width = $(id).width();
+      instance.height = $(id).height();
+      instance.speedX = speedX;
+      instance.speedY = speedY;
+      
+      return instance;
+    }
+  var snake = Factory("#snake", 0, 0, 0, 0);
   
-var boardWidth = $('#board').width();	
-var boardHeight = $('#board').height();
+  var apple = Factory("#apple", Math.floor(Math.random() * 800), Math.floor(Math.random() * 800));
+  
+  var boardWidth = $('#board').width();	
+  var boardHeight = $('#board').height();
 
-var snakeWidth = $('#snake').width();	
-var snakeHeight = $('#snake').height();
+  var snakeWidth = $('#snake').width();	
+  var snakeHeight = $('#snake').height();
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-  $(document).on('keyup', handleKeyUp);                           // change 'eventType' to the type of event you want to handle
-
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -64,6 +65,8 @@ var snakeHeight = $('#snake').height();
   function newFrame() {
     repositionAndRedrawGameItem();
     checkBoundaries();
+    eatApple(); 
+    createBoard(); 
   }
   
   /* 
@@ -71,19 +74,19 @@ var snakeHeight = $('#snake').height();
   */
   function handleKeyDown(event) {
     if (event.which === KEY.LEFT || event.which === KEY.A){
-  	  snake.speedX = -1;
+  	  snake.speedX = -20;
       snake.speedY = 0;
   	}
   	if (event.which === KEY.RIGHT || event.which === KEY.D){
-  	  snake.speedX = +1;
+  	  snake.speedX = +20;
       snake.speedY = 0;
 	  }
     if (event.which === KEY.UP || event.which === KEY.W){
-      snake.speedY = -1;
+      snake.speedY = -20;
       snake.speedX = 0;
     }
     if (event.which === KEY.DOWN || event.which === KEY.S){
-      snake.speedY = +1;
+      snake.speedY = +20;
       snake.speedX = 0;
 	  }
   }
@@ -103,7 +106,22 @@ var snakeHeight = $('#snake').height();
 		}
   }
 
-  
+  function eatApple () {
+    var $snake = $("#snake");
+    var $apple = $("#apple")
+    if (doCollide($snake, $apple) == false) {
+      console.log("test");
+      $("#apple").hide(); 
+    }
+  }   
+
+  function createBoard() {
+    popup.style.display = "none";
+    for (let i = 0; i < 100; i++) {
+      let div = document.createElement("div");
+      grid.appendChild(div);
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -112,9 +130,24 @@ var snakeHeight = $('#snake').height();
     snake.y += snake.speedY;
     $("#snake").css("left", snake.x);
     $("#snake").css("top", snake.y);
-    $
+    
+    $("#apple").css("left", apple.x);
+    $("#apple").css("top", apple.y);
   }
   
+  function spawnApple () {
+    var apple = Factory("#apple", Math.random() * boardWidth, Math.random() * boardHeight, 0, 0);
+
+  }
+
+  function doCollide(a, b) {
+    return !(
+        ((a.y + a.height) < (b.y)) ||
+        (a.y > (b.y + b.height)) ||
+        ((a.x + a.width) < b.x) ||
+        (a.x > (b.x + b.width))
+    );
+}
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
