9a7fae3cc01a9b8bbfc3573fc04a455fbe331e73
ryanmulkey <ryanmulkey17@gmail.com>
2022-06-17 04:30:14 +0000

    push

--- mei/mei.js
+++ mei/mei.js
@@ -1,2 +1,30 @@
-const d = new Date();
-d.getDate();
+$(document).ready(runProgram);
+
+function runProgram(){
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+
+console.log(dd);
+
+$("#1").hide();
+$("#2").hide();
+$("#3").hide();
+$("#4").hide();
+$("#5").hide();
+$("#6").hide();
+$("#7").hide();
+$("#8").hide();
+$("#9").hide();
+//$("#10").hide();
+$("#11").hide();
+$("#12").hide();
+$("#13").hide();
+$("#14").hide();
+$("#5").hide();
+
+
+
+if (dd == 17) {
+    $("#1").show();
+}
+}
